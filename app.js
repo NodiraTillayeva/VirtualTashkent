aab00242449ea46cfc72d253d7e66aef16daca3b
NodiraTillayeva <ntillayeva@gmail.com>
2024-10-24 19:43:21 +0900

    norm basemap

--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 const map = L.map('map').setView([41.3111, 69.2797], 13); // Tashkent
 
 // Add Esri National Geographic World Map as a custom tile layer
-L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
-    attribution: '&copy; <a href="https://www.esri.com">Esri</a> contributors',
-    maxZoom: 16
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.carto.com">CartoDB</a> contributors'
 }).addTo(map);
+
 
 // Define a custom icon
 const customIcon = L.icon({
     iconUrl: 'quest.png', // Replace with your custom image URL or file path
-    iconSize: [20, 20], // Size of the icon [width, height]
+    iconSize: [30, 30], // Size of the icon [width, height]
     iconAnchor: [16, 32], // Point of the icon which will correspond to marker's location
     popupAnchor: [0, -32] // Position of the popup relative to the icon
 });
